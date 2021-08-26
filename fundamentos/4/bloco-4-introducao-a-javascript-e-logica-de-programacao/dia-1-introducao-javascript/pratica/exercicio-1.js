db4a775280c58c218a21bf013db51f47093aa03a
Morallys <morallys.juan@gmail.com>
2021-08-26 11:35:33 -0300

    Alteração de local de arquivos dentro do dia 4.1

---
+++ fundamentos/4/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/pratica/exercicio-1.js
@@ -0,0 +1,34 @@
+// Adição (a+b)
+
+const a = 4;
+const b = 3;
+
+console.log(a + b);
+
+// Subtração
+
+const a = 4;
+const b = 3;
+
+console.log(b - a);
+
+// Multiplicação
+
+const a = 4;
+const b = 3;
+
+console.log(a * b);
+
+// Divisão
+
+const a = 12;
+const b = 3;
+
+console.log(a / b);
+
+// Módulo
+
+const a = 12;
+const b = 5;
+
+console.log(a % b);

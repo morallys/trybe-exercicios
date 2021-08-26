97c91fc745eabfb794d1deba34ca249cd27eaa73
Morallys <morallys.juan@gmail.com>
2021-08-25 23:29:00 -0300

    Inserção de pasta de Fundamentos completa

---
+++ 4/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/scripts.js
@@ -0,0 +1,64 @@
+// Exercício de fixação - 1
+
+// const myName = "Juan";
+// const birthCity = "Natal";
+// let birthYear = 1989;
+// birthYear = 2030;
+
+// console.log("Meu nome é,", myName, ", nasci em", birthCity, "em", birthYear);
+
+// Exercício de fixação - 2
+
+// const base = 5;
+// let height = 8;
+// const area = base * height;
+// const perimeter = ((base*2) + (height*2));
+
+// console.log("base: ", base);
+// console.log("altura: ", height);
+// console.log("area: ", area);
+// console.log("Perímetro: ", perimeter);
+
+// Exercício de fixação - 3
+
+const notaCanditada = 59;
+
+if (notaCanditada >= 80) {
+    console.log("Parabéns, você foi aprovada(o)");
+
+}else if (notaCanditada >= 60){
+    console.log("Você está na nossa lista de espera");
+
+}else {
+    console.log("Você foi reprovada(o)");
+}
+
+/*
+Tipos primitivos
+
+tipos de referencia e o tipo de valores
+*/
+
+// let movie = "Avengers"; //string liretal
+// let score = 10.89; //number literal
+// let isValid = true; //boolean
+// let name; //undefined
+// let color = null; //aguardando redefinição de valor
+
+// let salary = 3500;
+
+// salary++;
+
+// console.log(salary);
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientAge);
+
+// console.log(typeof patientId);

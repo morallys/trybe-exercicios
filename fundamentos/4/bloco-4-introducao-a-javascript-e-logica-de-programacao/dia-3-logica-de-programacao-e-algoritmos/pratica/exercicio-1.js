f0324e548429cd7e68a049088ac75755b1397628
Morallys <morallys.juan@gmail.com>
2021-08-30 15:58:11 -0300

    Adicionando exercícios práticos do dia 4.3

--- fundamentos/4/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/pratica/exercicio-1.js
+++ fundamentos/4/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmos/pratica/exercicio-1.js
@@ -9,38 +9,18 @@
                             ***** 
  */
 
-/**
- * 1 - dado um n qualquer maior que 1
- * 2 - criar um laço de repetição para repetir n vezes a linha
- * 3 - criar um laço para repetir na coluna
- * 4 - imprimir na tela n vezes em n linhas o valor n em *
- */
-
 const n = 5;
-let aux = [];
+let line = '';
+let character = '*';
 
 if (n > 1) {
     for (let x = 0; x < n; x += 1) {
-        //imprime a linha: como fazer pra não quebrar linha?
         for (let y = 0; y < n; y += 1) {
-            //pegar o aux e de acordo com o n, multiplicar ele para mostrar uma string com um certo número de caracteres
-            aux.push("*");   
+            line = line + character;  
         }       
-        console.log(aux); 
-        //zerar o array
+        console.log(line); 
+        line = '';
     }
+}else {
+    console.log("Valor inválido");
 }
-
-
-/**
- * if (n > 1) {
-    for (let x = 0; x < n; x += 1) {         
-        inputLine = inputLine + caractere;
-
-    }
-    for (let y = 0; y < n; y += 1) {
-        console.log(inputLine);
-        
-    }    
-}
- */

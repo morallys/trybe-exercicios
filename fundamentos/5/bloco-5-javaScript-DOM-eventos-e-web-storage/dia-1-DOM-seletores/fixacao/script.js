602a1df2bea0ab8d8f1c82de9661e3c1272e198c
Morallys <morallys.juan@gmail.com>
2021-09-03 02:19:11 -0300

    Organização de pastas e Alteração no arquivo aula-offline

---
+++ fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-1-DOM-seletores/fixacao/script.js
@@ -0,0 +1,37 @@
+/*document.getElementById('teste').innerText = 'Alterando o texto do documento pelo script';
+
+document.getElementById('teste2').innerText = 'Parágrafo alterado via script';*/
+
+document.getElementById("page-title").innerText = "Breaking bad";
+// let paragrafos = 
+document.getElementsByClassName("paragraphs")[0].style.backgroundColor = 'green';
+// let segundoParagrafo = document.getElementById('second-paragraph');
+let subtitulo = document.getElementsByTagName('h4');
+
+//paragrafos[0].
+
+//paragrafos.style.color = "purple";
+// segundoParagrafo.style.border = 'solid 2px red';
+// segundoParagrafo.style.color = 'green';
+document.body.style.color = 'white';
+document.body.style.backgroundColor = 'black';
+subtitulo[0].style.color = 'blue';
+subtitulo[0].style.textAlign = 'center';
+
+
+
+
+
+
+/**
+ * Agora você! Faça o seguinte:
+    1 - Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+
+    2 - Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+
+    3 - Por fim, recupere o subtítulo e altere-o também.
+
+    Há mais formas de você acessar o conteúdo dos elementos do seu HTML . Aí vão algumas outras!
+
+    Você pode acessar os elementos filtrando pelo nome da classe:
+ */

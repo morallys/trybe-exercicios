b54a55a810d03272ac7a3b2bb69d6a9be6976a3b
Morallys <morallys.juan@gmail.com>
2021-09-06 19:17:55 -0300

    Alterando e adicionando arquivos aula 5.1

--- fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-1-DOM-seletores/fixacao/script-part3.js
+++ fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-1-DOM-seletores/fixacao/script-part3.js
@@ -1,5 +1,27 @@
-let classEmergency = document.querySelectorAll('.emergency-tasks')[1];
-let classNoEmergency = document.querySelectorAll('.no-emergency-tasks')[1];
+let headerText = document.getElementById('header-container');
 
-body.classEmergency.style.float = 'rigth';
-body.classNoEmergency.style.float = 'rigth';
+headerText.style.backgroundColor = '#00B069';
+
+let sectionUrgencyTasks = document.getElementsByClassName('emergency-tasks');
+
+sectionUrgencyTasks[0].style.backgroundColor = '#FF9F84';
+
+let h3SectionUrgencyTasks = document.querySelectorAll('.emergency-tasks h3');
+
+for (const iterator of h3SectionUrgencyTasks) {
+    iterator.style.backgroundColor = '#A500F3'
+}
+
+let sectionNoUrgencyTasks = document.getElementsByClassName('no-emergency-tasks');
+
+sectionNoUrgencyTasks[0].style.backgroundColor = '#f9db5e';
+
+let h3SectionNoUrgencyTasks = document.querySelectorAll('.no-emergency-tasks h3');
+
+for (const iterator of h3SectionNoUrgencyTasks) {
+    iterator.style.backgroundColor = '#232525';
+}
+
+let footerContainer = document.querySelector('#footer-container');
+
+footerContainer.style.backgroundColor = '#003533';

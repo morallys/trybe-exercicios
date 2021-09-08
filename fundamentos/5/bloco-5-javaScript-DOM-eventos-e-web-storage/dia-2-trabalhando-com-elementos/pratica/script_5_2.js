1eafb6be79b8aeae14aca2dce7a9104ed59cfcb2
Morallys <morallys.juan@gmail.com>
2021-09-08 03:09:50 -0300

    Requisito 2 check

--- fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/pratica/script_5_2.js
+++ fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/pratica/script_5_2.js
@@ -1,4 +1,10 @@
+
+// Requisito 1
 let element_h1 = document.createElement('h1');
 element_h1.innerText = 'Exercício 5.2 - JavaScript DOM';
-
 document.body.appendChild(element_h1);
+
+// Requisito 2
+let element_main = document.createElement('main');
+element_main.className = 'main-content';
+document.body.appendChild(element_main);

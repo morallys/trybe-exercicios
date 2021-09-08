47da2f55a78ef6e2724180779cd6ec278c9e1230
Morallys <morallys.juan@gmail.com>
2021-09-08 03:15:10 -0300

    Requisito 3 check

--- fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/pratica/script_5_2.js
+++ fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-2-trabalhando-com-elementos/pratica/script_5_2.js
@@ -8,3 +8,9 @@ document.body.appendChild(element_h1);
 let element_main = document.createElement('main');
 element_main.className = 'main-content';
 document.body.appendChild(element_main);
+
+// Requisito 3
+let element_section = document.createElement('section');
+let element_pai_section = document.querySelector('.main-content')
+element_section.className = 'center-content';
+element_pai_section.appendChild(element_section);

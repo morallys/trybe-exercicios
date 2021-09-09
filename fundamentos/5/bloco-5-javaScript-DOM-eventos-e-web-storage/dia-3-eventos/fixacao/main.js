98d62f7959cebbb1e7f485dd3a398b8569f1ac10
Morallys <morallys.juan@gmail.com>
2021-09-09 02:55:28 -0300

    Adicionando arquivos de exercícios 5.3

---
+++ fundamentos/5/bloco-5-javaScript-DOM-eventos-e-web-storage/dia-3-eventos/fixacao/main.js
@@ -0,0 +1,16 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+// Requisito 2
+window.onload = userInput.addEventListener('data', function () {
+
+  let userInput = document.getElementById('data');
+  let inputText = userInput.innerText;
+
+  let textClassTech = document.querySelector('.tech');
+  textClassTech = userInput;
+
+})

8840b9a41bdc696ec47ef85a5429c37f69ba86b7
morallys <morallys.juan@gmail.com>
2021-11-29 12:31:23 -0300

    Adiciona arquivos bloco/dia 14.2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/fixacao/exercicio-1-reduce.js
+++
@@ -1,7 +0,0 @@
-// NOTE - Verificar qual é o maior número
-
-const numbers = [50, 85, -30, 3, 15];
-
-const biggerNumber = numbers.reduce((acc, number) => number > acc ? number : acc, 100);
-
-console.log(biggerNumber);

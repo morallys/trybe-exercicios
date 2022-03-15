ab5d9976e13601777420691f0570056776445af8
morallys <morallys.juan@gmail.com>
2022-03-15 12:59:23 -0300

    Adiciona exercicios aula 22.2

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/promisesexemplo/funcaoSemPromise.js
@@ -0,0 +1,16 @@
+const calcularDivisao = (num1, num2) => {
+  if (num2 === 0) throw new Error('Não pode ser feito uma divisão por zero.')
+
+  const resultado = num1 / num2;
+
+  return resultado;
+}
+
+try {
+  const resultado = calcularDivisao(6, 0);
+
+  console.log("resultado: %s", resultado);
+
+} catch (error) {
+  console.log("erro: %s", error.message);
+}

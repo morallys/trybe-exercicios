c437fe1b14addebbbe8101966e082f525fe9968f
morallys <morallys.juan@gmail.com>
2022-05-06 15:30:52 -0300

    Adiciona arquivos do dia 25.2

--- back-end/bloco-25-deployment/dia-2-deploy-docker-heroku/supabase-with-sequelize/src/routes/productRoutes.js
+++ back-end/bloco-25-deployment/dia-2-deploy-docker-heroku/supabase-with-sequelize/src/routes/productRoutes.js
@@ -5,7 +5,7 @@ const productController = require('../controllers/productController')
 const router = express.Router();
 
 router
-  .get('/', (req, res) => res.send('testando'))
+  .get('/', (req, res) => res.send('testando API pelo Heroku'))
   .post('/', productController.create);
 
 module.exports = router;
